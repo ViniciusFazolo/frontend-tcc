22f38ea538b36f55d43caaef82638f65f07014c7
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2025-06-11 17:02:03 -0300

    feat: deixa a stack mais personalizavel

--- src/components/stack/index.tsx
+++ src/components/stack/index.tsx
@@ -3,7 +3,7 @@ import { Pressable, View } from "react-native";
 import { Props } from "./interface/props";
 import { AntDesign } from "@expo/vector-icons";
 
-export default function Stack({children, href}: Props) {
+export default function Stack({children, href, bgColor = "bg-white", arrowColor = "black"}: Props) {
     const router = useRouter()
 
     function handleBackPage(){
@@ -11,12 +11,12 @@ export default function Stack({children, href}: Props) {
     }
 
   return (
-    <View className="bg-white w-full h-[70px] flex flex-row items-center text-gray-900 border-b shadow-[0_0_10px_rgba(0,0,0,0.05)] border-b-gray-200">
+    <View className={`${bgColor} w-full h-[70px] flex flex-row items-center text-gray-900 ${bgColor === "bg-transparent" ? '' : 'border-b-gray-200 border-b shadow-[0_0_10px_rgba(0,0,0,0.05)]'}`}>
       <Pressable
         onPress={handleBackPage}
         className="flex flex-row items-center"
       >
-        <AntDesign name="arrowleft" size={24} className="text-gray-900 p-5" />
+        <AntDesign name="arrowleft" size={24} className="text-gray-900 p-5"color={arrowColor} />
       </Pressable>
 
       {children}
--- src/components/stack/interface/props.ts
+++ src/components/stack/interface/props.ts
@@ -1,6 +1,8 @@
 import { Href } from "expo-router";
 
 export interface Props {
-    children: React.ReactNode,
-    href: Href
+    children?: React.ReactNode,
+    href: Href,
+    bgColor?: "bg-transparent" | 'bg-white',
+    arrowColor?: 'white' | 'black'
 }
